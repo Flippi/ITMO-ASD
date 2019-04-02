813be95066e865916603ba3f7176698a2c61ffad
Flippi <vovaregen2@mail.ru>
2019-04-02 10:57:48 +0300

    Add files via upload

---
+++ openedu/week5/week5_1.cpp
@@ -0,0 +1,31 @@
+#include "edx-io.hpp"
+using namespace std;
+
+
+int main() {
+
+	long N;
+	io >> N;
+	long* array = new long[N];
+
+
+	for (long i = 0; i < N; i++) {
+		io >> array[i];
+	}
+	bool isHeap = true;
+	//На каждом узле проверяем что его дети больше него
+	//Если хоть в 1 false - то это не куча
+	for (long i = 1; i < N / 2 + N % 2; i++) {
+		if (array[i - 1] > array[2 * i - 1] || array[i - 1] > array[2 * i]) {
+			isHeap = false;
+			break;
+		}
+	}
+	if (isHeap) {
+		io << "YES";
+	}
+	else {
+		io << "NO";
+	}
+	return 0;
+}
